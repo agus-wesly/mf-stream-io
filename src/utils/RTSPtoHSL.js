98c76b05cdfae621e43b174b97303e928c7dda36
aguswesly <agustchannel@gmail.com>
2024-02-22 11:15:45 +0700

    feat : adjust video resolution

--- src/utils/RTSPtoHSL.js
+++ src/utils/RTSPtoHSL.js
@@ -1,18 +1,15 @@
 Stream = require('node-rtsp-stream')
 stream = new Stream({
   name: 'Camera',
-  streamUrl: 'rtsp://eaglewing:eagle123wing@192.168.100.148:554/stream1', // Ganti dengan URL RTSP
+  streamUrl: 'rtsp://localhost:8554/mystream', // Ganti dengan URL RTSP
   wsPort: 6789,
   ffmpegOptions: {
-    // options ffmpeg flags
-    '-f': 'mpegts', // output file format.
+    '-f': 'mpegts', // Output file format
     '-codec:v': 'mpeg1video', // video codec
-    '-b:v': '1000k', // video bit rate
-    '-stats': '',
-    '-r': 25, // frame rate
-    '-s': '640x480', // video size
-    '-bf': 0,
-    // audio
+    '-b:v': '6000k', // Video bit rate (increase for better quality)
+    '-s': '1920x1080', // Video size
+    '-bf': 0, // Number of B-frames
+    '-r': 60, // Number of B-frames
     '-codec:a': 'mp2', // audio codec
     '-ar': 44100, // sampling rate (in Hz)(in Hz)
     '-ac': 1, // number of audio channels
